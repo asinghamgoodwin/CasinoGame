d95cb8f24d0ac74d838ca2fdfd29a8314d210ca5
asinghamgoodwin <asinghamgoodwin@gmail.com>
2015-05-25 12:14:03 +0000

    update

--- CasinoComputerPlayer.py
+++ CasinoComputerPlayer.py
@@ -27,19 +27,22 @@ def discardValue(card): #if you're trying to decide what card to discard, use th
     return cardsValue([card])
 
 def getComputerMove(player, table):
-    '''this function returns a tuple with (0) the type of move followed by (1) a tuple with:
-        (0) the card to be played from the hand
-        (1) the list of cards from the table to play
-        (2) what build, if any, is involved'''
+    ###this function returns a tuple with (0) the type of move followed by (1) a tuple with:
+    ###(0) the card to be played from the hand
+    ###(1) the list of cards from the table to play
+    ###(2) what build, if any, is involved
     
     discardChoices = player.hand[:] #you could discard any card in your hand
     takeChoices = {} #this will get populated with different possibilites for what to take
-    #buildChoices = []
+    buildChoices = {}
     buildRank = 0
     
     allCardCombinations = []
     for i in range(1,len(table.availableCards())+1):
         allCardCombinations += list(itertools.combinations(table.availableCards(),i))
+
+
+##------------ Populating the takeChoices dictionary -----------##
         
     takePossible = False #if this stays False, we'll discard
 
@@ -63,20 +66,77 @@ def getComputerMove(player, table):
 
         if cardCanTake == False:
             del takeChoices[card] #we can get rid of the blank dictionary entry if it turns out that card couldn't actually take anything
-    
-    if takePossible == False:
-        return ("Discard", (min(discardChoices, key=discardValue), [],0)) #discard the least valuable card
 
-    else:
+
+##------------ Populating the buildChoices dictionary -----------##
+
+    buildPossible = False #if this stays False, we'll discard
+
+    if len(player.hand) > 1: #you can't build unless you have another card to take with!
+        for card in player.hand: #this will be the card to take with next time
+            buildChoices[card] = []
+            cardCanTakeBuild = False
+            for combination in allCardCombinations:
+                otherCardsList = player.hand[:]
+                otherCardsList.remove(card)
+                
+                for otherCard in otherCardsList: #the other card from your hand has to be in the resulting build                
+                    if multiplesCheck(card.rank, list(combination)+[otherCard]): #if that combination would be a legal build to take with a card of this rank
+                        buildChoices[card].append(list(combination)+[otherCard]) #put it in the list (with the card to actually play this time at the end
+                        buildPossible = True #which means we can build (don't have to discard)...
+                        cardCanTakeBuild = True
+                    
+            if card.rank in player.currentBuilds: #if we're already building to that
+                for c in otherCardsList:
+                    if c.rank == card.rank: #if there's a second card of this rank still in your hand
+                        cardCanTakeBuild = True
+                        buildChoices[card].append([c]) #you could just add that to the build
+
+            if cardCanTakeBuild == False:
+                del buildChoices[card] #we can get rid of the blank dictionary entry if it turns out that card couldn't actually take a build of anything
+
+
+##------------ Deciding what move to do -----------##
+
+
+    if takePossible: #if we can take cards, first figure out what the best way to take cards is
         flattenedTakeChoices = []
         for Hcard in takeChoices.keys(): #of all the cards you can take with,
             for combo in takeChoices[Hcard]: #for each set of table cards that was takeable,
                 flattenedTakeChoices.append([Hcard]+combo) #make a new list that has all of those table cards plus the card from your hand
                                                             #because this is the entire set that will get added to our pile, so we want to maximize those points 
-        move = max(flattenedTakeChoices, key=cardsValue)
-        if move[0].rank in table.builds.keys():
-            buildRank = move[0].rank
-        return ("Take", (move[0], [i for i in move if i in table.availableCards()], buildRank)) #move[0] is the card from the hand
+        bestTakeMove = max(flattenedTakeChoices, key=cardsValue)
+
+    if buildPossible: #if we can build something, figure out which build is best
+        flattenedBuildChoices = []
+        for Hcard in buildChoices.keys(): #of all the cards you can take a future build with
+            for combo in buildChoices[Hcard]: #for each set of table cards that was buildable,
+                flattenedBuildChoices.append([Hcard]+combo) #make a new list that has all of those table cards plus the card from your hand
+                                                            #because this is the entire set that will get added to our pile next turn, so we want to maximize those points 
+        bestBuildMove = max(flattenedBuildChoices, key=cardsValue)
+
+    buildBetter = False
+    takeBetter = False
+
+    
+    if buildPossible and takePossible:
+        if cardsValue(bestBuildMove) > cardsValue(bestTakeMove):
+            buildBetter = True
+        else:
+            takeBetter = True
+
+
+    if takePossible and not buildPossible or takeBetter:
+        if bestTakeMove[0].rank in table.builds.keys():
+            buildRank = bestTakeMove[0].rank
+        return ("Take", (bestTakeMove[0], [i for i in bestTakeMove if i in table.availableCards()], buildRank)) #move[0] is the card from the hand
                                                                                                 #the list comprehension makes sure to leave out the build cards
+    elif buildPossible and not takePossible or buildBetter:
+        buildRank = bestBuildMove[0].rank
+        return ("Build", (bestBuildMove[-1], [i for i in bestBuildMove[1:] if i in table.availableCards()], buildRank)) #move[-1] is the card from the hand to play this turn
+                                                        #the list comprehension makes sure to leave out the build cards and the card to take with next time        
+        
+    else:
+        return ("Discard", (min(discardChoices, key=discardValue), [],0)) #discard the least valuable card
 
 
